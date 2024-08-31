15923fb8dc64645f4dafea179e34473c42a25acd
Edwin Ortiz <edarcode@gmail.com>
2024-08-31 10:26:52 -0500

    iniciando footer

--- src/App/Footer/Footer.tsx
+++ src/App/Footer/Footer.tsx
@@ -1,8 +1,9 @@
 import Networks from "../../components/navs/Networks/Networks";
+import css from "./css.module.css";
 
 export default function Footer() {
 	return (
-		<footer>
+		<footer className={css.footer}>
 			<p>© Casi todos los derechos reservados.</p>
 			<p>
 				Creado por <strong>edarcode</strong>
