4309f4f5532806eecfb0c0e37e219d33932e7e4b
Edwin Ortiz <edarcode@gmail.com>
2024-09-03 06:43:27 -0500

    animando logo

--- src/components/logos/Logo/Logo.tsx
+++ src/components/logos/Logo/Logo.tsx
@@ -5,13 +5,13 @@ import { motion } from "framer-motion";
 export default function Logo() {
 	return (
 		<motion.img
-			initial={{ opacity: 0, scale: 0.5 }}
-			animate={{ opacity: 1, scale: 1, rotate: 360 }}
-			whileHover={{ scale: 1.1 }}
-			transition={{ duration: 0.3 }}
 			className={css.logo}
 			src={logo}
 			alt="logo notaku"
+			initial={{ opacity: 0, scale: 0.5 }}
+			animate={{ opacity: 1, scale: 1, rotate: 360 }}
+			whileHover={{ scale: 1.1 }}
+			whileTap={{ scale: 0.5 }}
 		/>
 	);
 }
