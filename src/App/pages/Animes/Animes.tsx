525b8638a89d85fe1539ea5d39dad66b481a9c0c
Edwin Ortiz <edarcode@gmail.com>
2024-09-04 16:23:51 -0500

    mostrando los primeros animes en vista Animes

--- src/App/pages/Animes/Animes.tsx
+++ src/App/pages/Animes/Animes.tsx
@@ -1,11 +1,44 @@
+import css from "./css.module.css";
 import { useQuery } from "@tanstack/react-query";
 import { getAnimes } from "./services/getAnimes";
+import Spinner from "../../../components/spinners/Spinner/Spinner";
 
 export default function Animes() {
-	const { data: allAnimes } = useQuery({
-		queryKey: ["allAnimes"],
+	const {
+		isLoading,
+		isError,
+		data: kitsuAnimes
+	} = useQuery({
+		queryKey: ["kitsuAnimes"],
 		queryFn: () => getAnimes()
 	});
-	console.log(allAnimes);
-	return <div>Aún estamos trabajando en esta vista</div>;
+
+	console.log(kitsuAnimes);
+
+	if (isError) return <div className={css.err}>Error crgando animes</div>;
+	if (isLoading)
+		return (
+			<div className={css.loading}>
+				<Spinner />
+			</div>
+		);
+
+	if (!kitsuAnimes || !kitsuAnimes.animes.length)
+		return <div className={css.empty}>No hay animes</div>;
+
+	return (
+		<section className={css.animes}>
+			{kitsuAnimes.animes.map(anime => (
+				<div key={anime.id} className={css.anime}>
+					<div className={css.title}>{anime.title}</div>
+					<img
+						className={css.img}
+						src={anime.posterImage.small}
+						alt={anime.title}
+						loading="lazy"
+					/>
+				</div>
+			))}
+		</section>
+	);
 }
--- src/kitsu/urls.ts
+++ src/kitsu/urls.ts
@@ -1,7 +1,7 @@
 class Kitsu {
 	base = "https://kitsu.io/api/edge";
 	topAnimes = `${this.base}/anime?sort=popularityRank&page[limit]=10`;
-	animes = `${this.base}/anime`;
+	animes = `${this.base}/anime?sort=-startDate&page[limit]=16`;
 }
 
 export const KITSU = new Kitsu();
